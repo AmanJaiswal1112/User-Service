745a9d0b73afa25beab16ef97662ccd56c2d7549
AmanJaiswal1112 <aman.jaiswal.1112@gmail.com>
2023-03-25 20:10:32 +0530

    Added anotation

--- src/main/java/com/lewd/user/service/repository/UserRepository.java
+++ src/main/java/com/lewd/user/service/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.lewd.user.service.repository;
 
 import com.lewd.user.service.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
 }
