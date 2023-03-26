544f30f5c0640e807493024347525be87a23713b
AmanJaiswal1112 <aman.jaiswal.1112@gmail.com>
2023-03-26 19:18:56 +0530

    Implementation of Config-server

--- src/main/java/com/lewd/user/service/controller/UserController.java
+++ src/main/java/com/lewd/user/service/controller/UserController.java
@@ -2,7 +2,10 @@ package com.lewd.user.service.controller;
 
 import com.lewd.user.service.entity.User;
 import com.lewd.user.service.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +19,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user){
         User newUser = userService.saveUser(user);
@@ -23,10 +27,20 @@ public class UserController {
     }
 
     @GetMapping("/{userId}")
+    @RateLimiter(name = "userRateLimiter", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> createUser(@PathVariable String userId){
         User user = userService.getUserById(userId);
 
         return ResponseEntity.status(HttpStatus.OK).body(user);
+    }
+
+    public ResponseEntity<User> ratingHotelFallback(String userId, Exception ex) {
+       logger.info("Fallback is executed because service is down : ", ex.getMessage());
+
+        ex.printStackTrace();
+
+        User user = User.builder().email("dummy@gmail.com").name("Dummy").about("This user is created dummy because some service is down").userId("141234").build();
+        return new ResponseEntity<>(user, HttpStatus.BAD_REQUEST);
     }
 
     @GetMapping
